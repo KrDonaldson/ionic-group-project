6e989afda38660bf00eff8e046877f35cc8b8b84
KRISTINA DONALDSON <DONALDSONKRISTINAR@YAHOO.COM>
2020-01-12 14:46:07 -0800

    troubleshooting TodosPage errors

--- src/app/todos/todos.page.ts
+++ src/app/todos/todos.page.ts
@@ -1,34 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { Todo } from '../todo';
-import { TodoService } from '../todo.service';
+// import { Todo } from '../todo';
+// import { TodoService } from '../todo.service';
 
-export class TodoComponent {
+export class TodosPage {
 
-  newTodo: Todo = new Todo();
+  // newTodo: Todo = new Todo();
 
-  constructor(private _todoServe: TodoService) {
+  constructor() {
+    // private _todoServe: TodoService
+  // }
 
+  // addTodo() {
+  //   if (!this.newTodo.title) {
+  //     return;
+  //   }
+
+  //   this._todoServe.addTodo(this.newTodo);
+  //   this.newTodo = new Todo();
+  // }
+
+  // toggleTodoComplete(todo) {
+  //   this._todoServe.toggleTodoComplete(todo);
+  // }
+
+  // removeTodo(todo) {
+  //   this._todoServe.deleteTodoById(todo.id);
+  // }
+
+  // get todos() {
+  //   return this._todoServe.getAllTodos();
   }
-
-  addTodo() {
-    if (!this.newTodo.title) {
-      return;
-    }
-
-    this._todoServe.addTodo(this.newTodo);
-    this.newTodo = new Todo();
-  }
-
-  toggleTodoComplete(todo) {
-    this._todoServe.toggleTodoComplete(todo);
-  }
-
-  removeTodo(todo) {
-    this._todoServe.deleteTodoById(todo.id);
-  }
-
-  get todos() {
-    return this._todoServe.getAllTodos();
-  }
-
 }
