75c5f4397b81d9cda1e46d963dfcca4913c3da75
KRISTINA DONALDSON <DONALDSONKRISTINAR@YAHOO.COM>
2020-01-11 21:07:30 -0800

    todo service added. todos.page.ts copied from angular

--- src/app/todos/todos.page.ts
+++ src/app/todos/todos.page.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../todo.service';
+
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.page.html',
   styleUrls: ['./todos.page.scss'],
+  providers: [TodoService]
 })
 export class TodosPage implements OnInit {
 
-  constructor() { }
+  newTodo: Todo = new Todo();
+
+  constructor(private _todoServe: TodoService) { }
 
   ngOnInit() {
+  }
+
+  addTodo() {
+    if (!this.newTodo.title) {
+      return;
+    }
+
+    this._todoServe.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this._todoServe.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this._todoServe.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this._todoServe.getAllTodos();
   }
 
 }
