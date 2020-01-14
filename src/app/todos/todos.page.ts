9e99e23b4be756d1a7a5f14fbc7e1249e6ef8b85
KRISTINA DONALDSON <DONALDSONKRISTINAR@YAHOO.COM>
2020-01-13 19:26:23 -0800

    corrected component

--- src/app/todos/todos.page.ts
+++ src/app/todos/todos.page.ts
@@ -1,5 +1,6 @@
 import { TodoServiceService } from './../todo-service.service';
 import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.page.html',
